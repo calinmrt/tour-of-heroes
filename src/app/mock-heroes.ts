75bc424c855a9997ffbde8c03f9d7552bca82826
calin.marta <calin.marta@accesa.eu>
2019-12-08 11:05:26 +0200

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'superman'},
+  {id: 12, name: 'spiderman'},
+  {id: 13, name: 'wolverine'},
+  {id: 14, name: 'beast'},
+  {id: 15, name: 'gambet'},
+  {id: 16, name: 'batman'},
+  {id: 17, name: 'wonderwoman'},
+  {id: 18, name: 'hulk'},
+  {id: 19, name: 'ironman'},
+  {id: 20, name: 'thor'}
+]
